3a999bf3e91d1c212c90d71f7dbf584f0022b567
mattcroat <mattcroat.dev@gmail.com>
2022-03-01 20:17:42 +0100

    chore: Add site config

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const api = 'https://api.github.com'
+export const owner = 'mattcroat'
+export const repo = 'github-cms'
+export const path = 'posts'
+
+export const postsUrl = `${api}/repos/${owner}/${repo}/contents/${path}`
