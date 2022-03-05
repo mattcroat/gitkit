1e3697f1b00527e95d37e3228ccd0c29c819828e
mattcroat <mattcroat.dev@gmail.com>
2022-03-05 20:00:40 +0100

    feat: Add posts

---
+++ src/routes/editor/create/index.svelte
@@ -0,0 +1,56 @@
+<h1>Add Post</h1>
+
+<form action="/editor/create" method="post">
+	<div class="field">
+		<label for="slug">Slug</label>
+		<input type="text" name="slug" />
+	</div>
+
+	<div class="field">
+		<label for="content">Content</label>
+		<textarea
+			value="---
+title: Test
+description: Test
+slug: 'test'
+published: '2022-1-1'
+---"
+			name="content"
+			id="content"
+			cols="30"
+			rows="10"
+		/>
+	</div>
+
+	<button type="submit">+ Add post</button>
+</form>
+
+<style>
+	form {
+		display: grid;
+		gap: 1rem;
+	}
+
+	.field {
+		display: grid;
+		gap: 1rem;
+	}
+
+	input,
+	textarea {
+		padding: 1rem;
+		border-radius: 4px;
+		border: none;
+	}
+
+	button {
+		padding: 1.4rem;
+		font-size: inherit;
+		font-weight: 700;
+		color: inherit;
+		background: black;
+		border: none;
+		border-radius: 1rem;
+		cursor: pointer;
+	}
+</style>
---
+++ src/routes/editor/index.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let posts = []
+</script>
+
+<main>
+	<section>
+		<h1>Create Post</h1>
+		<a href="/editor/create">+ Create Post</a>
+	</section>
+
+	<section class="posts">
+		<h2>Posts</h2>
+		<ul class="posts">
+			{#each posts as post}
+				<li>
+					<a href="/editor/{post}">{post}</a>
+				</li>
+			{/each}
+		</ul>
+	</section>
+</main>
+
+<style>
+	main {
+		display: grid;
+		gap: 2rem;
+		padding: 2rem;
+	}
+
+	li {
+		display: flex;
+		gap: 2rem;
+	}
+</style>
