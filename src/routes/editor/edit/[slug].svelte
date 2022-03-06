7ada8be781fcd5ed74ab3417c8c0b9f30d4cd43f
mattcroat <mattcroat.dev@gmail.com>
2022-03-06 23:01:00 +0100

    feat: Editor

--- src/routes/editor/edit/[slug].svelte
+++ src/routes/editor/edit/[slug].svelte
@@ -1,50 +1,68 @@
 <script lang="ts">
+	import snarkdown from 'snarkdown'
+
 	export let slug = ''
 	export let content = ''
+
+	$: preview = content.replace(/^---[\s\S]*---$/gm, '')
 </script>
 
-<h1>Edit post</h1>
+<h2>Edit post</h2>
 
-<form method="post">
-	<div class="field">
-		<label for="slug">Slug</label>
-		<input type="text" name="slug" value={slug} />
-	</div>
+<main>
+	<form method="post">
+		<input type="hidden" name="slug" value={slug} />
+		<textarea
+			aria-label="Markdown editor"
+			class="editor"
+			bind:value={content}
+			name="content"
+			id="content"
+		/>
+		<button class="save" type="submit">💾 Save</button>
+	</form>
 
-	<div class="field">
-		<label for="content">Content</label>
-		<textarea value={content} name="content" id="content" cols="30" rows="10" />
-	</div>
-
-	<button type="submit">Save</button>
-</form>
+	<section class="preview">
+		{@html snarkdown(preview)}
+	</section>
+</main>
 
 <style>
+	h2 {
+		padding: 1rem 2rem;
+	}
+
+	main {
+		height: 100%;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+	}
+
 	form {
-		display: grid;
-		gap: 1rem;
+		position: relative;
 	}
 
-	.field {
-		display: grid;
-		gap: 1rem;
-	}
-
-	input,
-	textarea {
-		padding: 1rem;
-		border-radius: 4px;
+	.editor {
+		width: 100%;
+		height: 100%;
+		padding: 1rem 2rem;
+		font-size: 1.8rem;
+		font-family: 'Poppins', sans-serif;
+		color: hsl(0 0% 98%);
+		background-color: hsl(220 20% 8%);
 		border: none;
+		outline: none;
+		resize: none;
 	}
 
-	button {
-		padding: 1.4rem;
-		font-size: inherit;
-		font-weight: 700;
-		color: inherit;
-		background: black;
-		border: none;
-		border-radius: 1rem;
-		cursor: pointer;
+	.save {
+		position: absolute;
+		left: 1rem;
+		bottom: 1rem;
+	}
+
+	.preview {
+		padding: 1rem 2rem;
+		background-color: hsl(220 20% 10%);
 	}
 </style>
