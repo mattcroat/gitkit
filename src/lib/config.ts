83af06c1f981a6f9a7412a6f436318e38e2563ee
mattcroat <mattcroat.dev@gmail.com>
2022-03-21 13:05:06 +0100

    chore: Update site config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,10 @@
 export const api = 'https://api.github.com'
 export const owner = 'mattcroat'
-export const repo = 'github-cms'
+export const repo = 'gitkit'
 export const path = 'posts'
 
+// https://api.github.com/repos/mattcroat/gitkit/contents/posts/post.md
 export const postsUrl = `${api}/repos/${owner}/${repo}/contents/${path}`
+
+// https://github.com/mattcroat/gitkit/blob/main/posts/post.md
 export const fileUrl = `https://github.com/${owner}/${repo}/blob/main/posts`
