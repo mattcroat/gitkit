646afc1d1193c5d94c23f6533c2c6b8d6b6bd781
mattcroat <mattcroat.dev@gmail.com>
2022-03-23 14:52:10 +0100

    refactor: Use Heroicons icon library

--- src/routes/editor/index.svelte
+++ src/routes/editor/index.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import {
+		PlusCircleIcon,
+		TrashIcon
+	} from '@rgossiaux/svelte-heroicons/outline'
+
 	import { enhance } from '$root/lib/form'
 	import type { PostItemType } from '$root/types'
 
@@ -11,22 +16,7 @@
 		<h2>Create Post</h2>
 		<div class="create">
 			<a href="/editor/create">Create post</a>
-			<svg
-				width="24"
-				height="24"
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				stroke-width="2"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-				/>
-			</svg>
+			<PlusCircleIcon width="24" height="24" />
 		</div>
 	</section>
 
@@ -42,21 +32,7 @@
 						<input type="hidden" name="draft" />
 						<input type="hidden" name="slug" value={draft.slug} />
 						<button type="submit">
-							<svg
-								width="24"
-								height="24"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								stroke-width="2"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-								/>
-							</svg>
+							<TrashIcon width="24" height="24" />
 						</button>
 					</form>
 				</li>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { DocumentTextIcon } from '@rgossiaux/svelte-heroicons/outline'
 	import type { PostItemType } from '$root/types'
 
 	export let posts: PostItemType[] = []
@@ -8,21 +9,7 @@
 	<h2>Posts</h2>
 	{#each posts as post}
 		<article class="post">
-			<svg
-				width="24"
-				height="24"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				stroke-width="2"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-				/>
-			</svg>
+			<DocumentTextIcon width="24" height="24" />
 			<a href={post.slug} sveltekit:prefetch>{post.title}</a>
 		</article>
 	{/each}
