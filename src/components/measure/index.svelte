0b6e169470e4f9e948b51182daa07a13efb11aec
mattcroat <mattcroat.dev@gmail.com>
2022-03-29 16:25:09 +0200

    chore: Update rate limit HTML

--- src/components/measure/index.svelte
+++ src/components/measure/index.svelte
@@ -12,10 +12,8 @@
 	{#await rate()}
 		<p>⏳️</p>
 	{:then rate}
-		<p><span>{rate.limit}</span> limit</p>
-		<p><span>{rate.used}</span> used</p>
-		<p><span>{rate.resetTimeLocale}</span> reset</p>
-		<p><span>{rate.remainingMinutes}</span> minutes until reset</p>
+		<span>⭐️ {rate.used}/{rate.limit}</span>
+		<span>⏳️ {rate.remainingMinutes}</span> min
 	{:catch}
 		<p>💩</p>
 	{/await}
@@ -25,10 +23,14 @@
 	.rate {
 		--space: 20px;
 
-		position: absolute;
-		bottom: var(--space);
-		left: var(--space);
+		position: fixed;
+		top: var(--space);
+		right: var(--space);
 		z-index: 1;
+
+		padding: var(--spacing-16);
+		background-color: hsl(220 20% 12%);
+		border-radius: var(--radius-1);
 	}
 
 	span {
