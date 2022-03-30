692125c495000d2a0475a093135c848ce7e8e0ec
mattcroat <mattcroat.dev@gmail.com>
2022-03-30 15:46:57 +0200

    feat: Font size and text length control

--- src/components/settings/index.svelte
+++ src/components/settings/index.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Font from './font.svelte'
+	import Text from './text.svelte'
 	import Themes from './themes.svelte'
 </script>
 
 <Themes />
 <Font />
+<Text />
---
+++ src/components/settings/text.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { browser } from '$app/env'
+
+	const rootElement = browser ? document.documentElement : null
+	let font = browser ? localStorage.size.replace('px', '') : 18
+	let text = browser ? localStorage.text.replace('ch', '') : 60
+
+	function handleFontSizeChange() {
+		localStorage.size = `${font}px`
+		rootElement.style.setProperty('--post-font-size', `${font}px`)
+	}
+
+	function handleTextLengthChange() {
+		localStorage.text = `${text}ch`
+		rootElement.style.setProperty('--post-reading-length', `${text}ch`)
+	}
+</script>
+
+<div class="slider">
+	<label for="font">
+		<span class="unit">{font}px</span> font size
+	</label>
+	<input
+		on:change={handleFontSizeChange}
+		bind:value={font}
+		type="range"
+		name="font"
+		id="font"
+		min="16"
+		max="24"
+		step="1"
+	/>
+</div>
+
+<div class="slider">
+	<label for="text">
+		<span class="unit">{text}ch</span> text length
+	</label>
+	<input
+		on:change={handleTextLengthChange}
+		bind:value={text}
+		type="range"
+		name="text"
+		id="text"
+		min="60"
+		max="100"
+		step="10"
+	/>
+</div>
+
+<style>
+	.slider {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.unit {
+		font-weight: 700;
+	}
+</style>
